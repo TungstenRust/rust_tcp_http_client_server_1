e96b407239c468b3f0a54cef7a7e08b3c83a9914
Tungsten Rust <tungsten.rust@gmail.com>
2022-04-23 01:19:26 -0700

    comments added for clarification

--- http/src/http_request.rs
+++ http/src/http_request.rs
@@ -1,3 +1,56 @@
+// The Structure of HTTP request
+use std::collections::HashMap;
+#[derive(Debug, PartialEq)]
+pub enum Resource {
+    Path(String),
+}
+
+#[derive(Debug)]
+pub struct HttpRequest {
+    pub method: Method,
+    pub version: Version,
+    pub resource: Resource,
+    pub headers: HashMap<String, String>,
+    pub msg_body: String,
+}
+// Parsing incoming HTTP requests: via from() method
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_version = Version::V1_1;
+        let mut parsed_resource = Resource::Path("".to_string());
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
+
+        // Read each line in the incoming HTTP request
+        for line in req.lines() {
+            // If the line read is request line, call function process_req_line()
+            if line.contains("HTTP") {
+                let (method, resource, version) = process_req_line(line);
+                parsed_method = method;
+                parsed_version = version;
+                parsed_resource = resource;
+                // If the line read is header line, call function process_header_line()
+            } else if line.contains(":") {
+                let (key, value) = process_header_line(line);
+                parsed_headers.insert(key, value);
+                //  If it is blank line, do nothing
+            } else if line.len() == 0 {
+                // If none of these, treat it as message body
+            } else {
+                parsed_msg_body = line;
+            }
+        }
+        // Parse the incoming HTTP request into HttpRequest struct
+        HttpRequest {
+            method: parsed_method,
+            version: parsed_version,
+            resource: parsed_resource,
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
+    }
+}
 #[derive(Debug, PartialEq)]
 pub enum Method {
     Get,
