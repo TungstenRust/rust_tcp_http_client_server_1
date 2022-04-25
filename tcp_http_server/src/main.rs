190f0e8df68fd7cf617779eb00c72d38901f89ca
Tungsten Rust <tungsten.rust@gmail.com>
2022-04-25 16:22:20 -0700

    http server is ready

--- tcp_http_server/src/main.rs
+++ tcp_http_server/src/main.rs
@@ -1,3 +1,10 @@
+mod handler;
+mod server;
+mod router;
+use server::Server;
 fn main() {
-    println!("Hello, world!");
+    // Start a server
+    let server = Server::new("localhost:9000");
+    //Run the server
+    server.run();
 }
